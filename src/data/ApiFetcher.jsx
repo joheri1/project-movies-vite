73592ad0011a2064b5448263cf8bb1f79f2eb5d0
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-11-13 21:03:52 +0100

    Fetch movie titles and display on App.jsx

--- src/data/ApiFetcher.jsx
+++ src/data/ApiFetcher.jsx
@@ -6,29 +6,42 @@
 // 1. The API key (Authorization token) is stored in an environment variable (`VITE_REACT_APP_MOVIE_API`)
 // 2. The token is accessed via `import.meta.env` to retrieve the key from the environment configuration instead of hardcoded directly in the code.
 
-const ApiFetcher = () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_MOVIE_API}`
-    }
-  };
+import { useEffect, useState } from "react";
 
-  // Fetch popular movies 
+const ApiFetcher = () => {
+  const [titles, setTitles] = useState([])
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_MOVIE_API}`
+      }
+    };
+
+    // Fetch popular movies 
   
-  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-    .then(res => res.json())
-    .then(data => {
-      console.log('Popular movies:', data.results);
-    })
-    .catch(err => console.error('Fetch error:', err));
+    fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+      .then(res => res.json())
+      .then(data => {
+        console.log('Popular movies:', data.results);
+        const movieTitles = data.results.map(movie => movie.original_title);
+        setTitles(movieTitles);
+      })
+      .catch(err => console.error('Fetch error:', err));
+  }, []);
   
   // Return something
 
   return (
     <div>
       <p>Fetching popular movies...</p>
+      <ul>
+        {titles.map((title, index) => (
+          <li key={index}>{title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
