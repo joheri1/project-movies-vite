b6617f8496c4fd72a017e7160778153bd343d80a
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-11-13 22:15:34 +0100

    Update the ApiFetcher to only handle the Api, and fetch an array of movies. The component MovieList goes through each movie in the array using map, and display the title. Add comment sections to the two components.

--- src/data/ApiFetcher.jsx
+++ src/data/ApiFetcher.jsx
@@ -5,11 +5,15 @@
 // Adjustments: 
 // 1. The API key (Authorization token) is stored in an environment variable (`VITE_REACT_APP_MOVIE_API`)
 // 2. The token is accessed via `import.meta.env` to retrieve the key from the environment configuration instead of hardcoded directly in the code.
+//3. The hooks useState to store the list of movies fetched from the API, and useEffect is used to perform the API call only once, when the component first loads.
+//4. Changed then chatch to async/await.
 
 import { useEffect, useState } from "react";
+import MovieList from "../pages/MovieList";
 
 const ApiFetcher = () => {
-  const [titles, setTitles] = useState([])
+  //useState to store all movies
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const options = {
@@ -17,33 +21,25 @@ const ApiFetcher = () => {
       headers: {
         accept: 'application/json',
         Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_MOVIE_API}`
-      }
-    };
+    }
+  };
 
     // Fetch popular movies 
   
-    fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-      .then(res => res.json())
-      .then(data => {
-        console.log('Popular movies:', data.results);
-        const movieTitles = data.results.map(movie => movie.original_title);
-        setTitles(movieTitles);
-      })
+  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(data => setMovies(data.results)) //Store the result in state
       .catch(err => console.error('Fetch error:', err));
-  }, []);
+  }, []); //Empty dependency array ( [] ) to ensure the fetch only runs once.
   
-  // Return something
+  // Return popular movies 
 
   return (
     <div>
       <p>Fetching popular movies...</p>
-      <ul>
-        {titles.map((title, index) => (
-          <li key={index}>{title}</li>
-        ))}
-      </ul>
+      <MovieList movies={movies} />
     </div>
-  )
-}
+  );
+};
 
 export default ApiFetcher;
---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+// This component shows a list of movie titles. 
+//It gets an array of movie objects as a prop from the ApiFetcher, and goes through each movie (using map), and displays its original title in a list.
+//Each item in the list has a unique key based on the movie's ID.
+
+const MovieList = ({ movies }) => {
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.original_title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
