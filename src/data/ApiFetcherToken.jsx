141aaf5bc8a6469bd878bea6e73146dedd2fca19
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-11-14 16:02:01 +0100

    Update comment in ApiFetcherToken component.

--- src/data/ApiFetcherToken.jsx
+++ src/data/ApiFetcherToken.jsx
@@ -6,7 +6,6 @@
 // 1. The API token (Authorization token) is stored in an environment variable (`VITE_REACT_APP_MOVIE_API`)
 // 2. The token is accessed via `import.meta.env` to retrieve the key from the environment configuration instead of hardcoded directly in the code.
 //3. The hooks useState to store the list of movies fetched from the API, and useEffect is used to perform the API call only once, when the component first loads.
-//4. Changed then chatch to async/await.
 
 import { useEffect, useState } from "react";
 import MovieList from "../pages/MovieList";
