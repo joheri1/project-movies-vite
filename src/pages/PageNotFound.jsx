8e2f3cb57dc6a87e7c28b95a9a0e007f6e3bab60
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-11-14 20:00:36 +0100

    Define the PageNotFound component. Add routes to Routes component

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,23 @@
+// This component displays a "Page Not Found" message when the user navigates to a non-existent route. 
+// It imports Link from react-router-dom to create a navigation link for Go Back to Home.  
+
+import { Link } from "react-router-dom";
+
+export const PageNotFound = () => {
+  return (
+    <div>
+      {/* Main heading for the error message */}
+      <h1>Oh no!</h1>
+
+      {/* Subheading to clarify that the page was not found */}
+      <h2>Page not found!</h2>
+
+      {/* Message explaining the situation to the user */}
+      <p>It seems that the page you are trying to access does not exist.</p>
+
+      {/* Link component to navigate back to the home page */}
+      {/* The link points to "/go-back-to-home", which is routed to render the Home component */}
+      <Link to="/go-back-to-home">Go Back to Home</Link>
+    </div>
+  );
+};
